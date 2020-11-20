e4c703fc77e2ccb446ac4d46168cd4e76d14fa68
照屋　勇真 <ky19860120@gmail.com>
2020-11-20 14:52:09 +0900

    SpellListスタイル　追加２

--- src/components/clipboard/SpellList.vue
+++ src/components/clipboard/SpellList.vue
@@ -2,48 +2,49 @@
     <div>
         
         <ul class="role-list">
-            <template v-for="role in roles">
-                <li class="role-item" @click="changeTab(role.id)" :key="role.id">{{ role.name }}</li>
+            <template v-for="(role, index) in roles">
+                <li class="role-item" :class="(index === active) ? 'active': '' " @click="changeTab(role.id)" :key="role.id">{{ role.name }}</li>
             </template>
         </ul>
+
         <div class="bg">
-        <template v-for="(ch, index) in chooseChar">
-            <template v-if="index === active">
-                <div :key="ch.id" class="spell">
+            <template v-for="(ch, index) in chooseChar">
+                <template v-if="index === active">
+                    <div :key="ch.id" class="spell">
 
-                    <ul class="spell-list">
-                        <template v-for="spell in spellList">
-                            <li 
-                                class="spell-item" 
-                                :key="spell.id"
-                                @click="setTimeCount(spell)"
-                            >
-                            <div>
-                                <p class="spell-item__txt">{{spell.name}}</p>
-
-                                <div class="spell-item__img">
-                                    <img :src="require(`@/assets/images/${spell.img}`)" alt="">
-                                </div>
+                        <div class="spell-ch">
+                            <div class="spell-ch__img">
+                                <img :src="require(`@/assets/images/${ch.img}`)" alt="">
                             </div>
-                            </li>
-                        </template>
-                    </ul>
+                            <p @click="setTimeCount(ch)">{{ch.CD[0]}}</p>
+                        </div>
 
-                    <div class="spell-ch">
-                        <p>{{ch.chanpion}}</p>
+                        <ul class="spell-list">
+                            <template v-for="spell in spellList">
+                                <li 
+                                    class="spell-item" 
+                                    :key="spell.id"
+                                    @click="setTimeCount(spell)"
+                                >
+                                    <p class="spell-item__txt">{{spell.name}}</p>
 
-                        <p @click="setTimeCount(ch)">{{ch.CD[0]}}</p>
+                                    <div class="spell-item__img">
+                                        <img :src="require(`@/assets/images/${spell.img}`)" alt="">
+                                    </div>
+                                </li>
+                            </template>
+                        </ul>
+
                     </div>
-
-                </div>
+                </template>
             </template>
-        </template>
-        
-        <div class="result" @click="addClipBoard($event)">
-            <p class="result__txt">{{ result }}aaaaaaaaa</p>
-            <i>O</i>
+            
+            <div class="result" @click="addClipBoard($event)">
+                <p class="result__txt">{{ result }}</p>
+                <i>O</i>
+            </div>
         </div>
-        </div>
+
     </div>
 </template>
 
@@ -107,45 +108,48 @@ export default {
     border-top: 1px solid black;
     border-right: 1px solid black;
     border-bottom: 1px solid black;
-    border-radius: 3px;
+    border-radius: 3px 3px 0 0;
     background: gray;
-    width: 56px;
+    width: 97px;
     height: 24px;
     text-align: center;
     line-height: 24px;
+    cursor: pointer;
 }
-.role-item:nth-child(1) {
+.role-item.active {
     border-bottom: 1px solid gray;
+    transition: all 0.1s ease-out;
 }
 .spell {
     display: flex;
-    box-sizing: border-box;
-    padding: 16px;
-    background: gray;
+    flex-direction: row-reverse;
     border-radius: 5px;
 }
 .spell-list {
     display: flex;
     flex-wrap: wrap;
-    width: 63%;
+    flex-basis: 63;
     height: 260px;
 }
 .spell-item {
     margin-right: 5px;
     margin-bottom: 5px;
-    width: 63px;
-    height: 63px;
+    width: 75px;
 }
 .spell-item__txt {
     text-align: center;
-    padding: 5px;
+    margin-bottom: 5px;
 }
 .spell-item__img {
     width: 100%;
-    height: 100%;
 }
 .spell-ch {
     text-align: center;
+    flex-basis: 37%;
+}
+.spell-ch__img {
+    border-radius: 50%;
+    overflow: hidden;
 }
 .result {
     display: flex;
@@ -162,5 +166,8 @@ export default {
 .bg {
     width: 100%;
     background: gray;
+    box-sizing: border-box;
+    padding: 15px;
+    border-radius: 0 5px 5px 5px;
 }
 </style>
